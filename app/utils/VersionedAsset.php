e152f442abd499852ce1cb530cad5d5d65fdbcb5
Nick Freear <nfreear@yahoo.co.uk>
2018-02-05 22:44:00 +0000

    Bug #33, Use `javascript` filter for 'd3' etc- `view` Twig templates
    * .. 'datatables', 'jquery-blockui' ... [iet:10299495][ci skip]

--- app/utils/VersionedAsset.php
+++ app/utils/VersionedAsset.php
@@ -32,9 +32,15 @@ class VersionedAsset extends \Application {
     'jqueryui' => 'https://unpkg.com/jqueryui@{ver}/jquery-ui.min.js',
     'jquery-dotdotdot' => '{assets}/dotdotdot-1.6.3/jquery.dotdotdot.min.js',
     '_X_CDN_jquery-dotdotdot' => 'https://unpkg.com/jquery-dotdotdot@{ver}/src/jquery.dotdotdot.min.js',
-    'jquery-blockui' => '{assets}/jquery-ui-1.9.2.custom/js/jquery.blockUI.js',
-    'd3' => '{assets}/d3.js/d3.v3.min.js',
-    'countable' => 'https://unpkg.com/countable@3.0.0/Countable.min.js',
+    'jquery-blockui' => '{assets}/jquery-ui-1.9.2.custom/js/jquery.blockUI.js', // X 7;
+    '_X_LOCAL_d3' => '{assets}/d3.js/d3.v3.min.js',
+    'd3' => 'https://unpkg.com/d3@{ver}/d3.min.js', // X 10;
+    'd3-colorbrewer' => '{assets}/d3.js/colorbrewer.js' // X 10;
+    // 'highcharts'
+    'datatables' => '{assets}/jquery.dataTables/js/jquery.dataTables.min.js',
+    '_X_CDN_datatables' => 'https://unpkg.com/datatables@1.10.13/media/js/jquery.dataTables.min.js',
+    'countable' => 'https://unpkg.com/countable@{ver}/Countable.min.js',
+    'x-exhibit-api' => 'https://api.simile-widgets.org/exhibit/3.0.0/exhibit-api.js',
   ];
 
   protected static $depends = [];
@@ -49,7 +55,7 @@ class VersionedAsset extends \Application {
   }
 
   /**
-   * @param string $key  Get the URL for a Javascript, by key, eg. 'jquery'.
+   * @param string $key  Get the URL for a Javascript by key, eg. 'jquery'.
    * @return string
    */
   public function getJavascriptUrl( $key )
@@ -59,7 +65,7 @@ class VersionedAsset extends \Application {
   }
 
   /**
-   * @param string $key  Get the URL for a stylesheet, by key, eg. 'twitter-bootstrap'.
+   * @param string $key  Get the URL for a stylesheet by key, eg. 'twitter-bootstrap'.
    * @return string
    */
   public function getStylesheetUrl( $key )
