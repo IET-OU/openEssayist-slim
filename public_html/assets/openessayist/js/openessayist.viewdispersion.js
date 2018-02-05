34541d7717884cf962b068c9ea523037ee821008
Nick Freear <nfreear@yahoo.co.uk>
2018-02-05 15:16:41 +0000

    Bug #33, whitespace fix for `viewDispersion` Javascript [iet:10299495][ci skip]

--- public_html/assets/openessayist/js/openessayist.viewdispersion.js
+++ public_html/assets/openessayist/js/openessayist.viewdispersion.js
@@ -32,7 +32,7 @@ window.$.fn.DataTable._fnBindAction = function (n, oData, fn) {
 };
 
 (function (W, $, openEssayist) {
-// $(document).ready(function() {
+// $(document).ready(function () {
   var chart = null;
   var chartOptions = null;
 
@@ -119,192 +119,194 @@ window.$.fn.DataTable._fnBindAction = function (n, oData, fn) {
     return clr2;
   }
 
-  var tt = getColor('#+s:i#');
+  // var tt = getColor('#+s:i#');
   // var myCats = DISP.myCats; // {{ categories|json_encode|raw }}; // NDF:
 
   openEssayist._VD_initialize = function (DISP) {
-  chartOptions = {
-    credits: {
-      enabled: false
-    },
-    legend: {
-      enabled: false,
-      useHTML: true,
-      itemStyle: {
-        color: '#000000'
-      }
-    },
-    exporting: {
-      enabled: false,
-      sourceWidth: 1175,
-      sourceHeight: 825
-    },
-    chart: {
-      backgroundColor: null,
-      renderTo: 'container',
-      type: 'scatter',
-      zoomType: 'x',
-      reflow: true,
-      resetZoomButton: {
-        theme: {
-          display: 'none'
-        }
-      }
-    },
-    title: {
-      text: 'Occurrence of key words & key phrases throughout the essay',
-      useHTML: true
-    },
-    tooltip: {
-      followPointer: true,
-      followTouchMove: true,
-      hideDelay: 100,
-
-      formatter: function () {
-        return '<b>' + DISP.myCats[ this.point.y ] + '</b><br>' + this.series.name;
-      }
-    },
-    xAxis: [{ // master axis
-      min: 0,
-      title: {
-        enabled: false,
-        text: 'word count'
-      },
-      labels: {
+    chartOptions = {
+      credits: {
         enabled: false
       },
-      minRange: 200,
-      startOnTick: false,
-      endOnTick: false,
-      showLastLabel: true,
-      plotBands: DISP.plotBands // NDF:
-      /* plotBands: [
-        {% for key,band in structure %}
-          {
-            from: {{ band.from }},
-            to: {{ band.to }},
-            color: getColor('{{ band.tag }}'),
-             thickness: '5%'
+      legend: {
+        enabled: false,
+        useHTML: true,
+        itemStyle: {
+          color: '#000000'
+        }
+      },
+      exporting: {
+        enabled: false,
+        sourceWidth: 1175,
+        sourceHeight: 825
+      },
+      chart: {
+        backgroundColor: null,
+        renderTo: 'container',
+        type: 'scatter',
+        zoomType: 'x',
+        reflow: true,
+        resetZoomButton: {
+          theme: {
+            display: 'none'
+          }
+        }
+      },
+      title: {
+        text: 'Occurrence of key words & key phrases throughout the essay',
+        useHTML: true
+      },
+      tooltip: {
+        followPointer: true,
+        followTouchMove: true,
+        hideDelay: 100,
+
+        formatter: function () {
+          return '<b>' + DISP.myCats[this.point.y] + '</b><br>' + this.series.name;
+        }
+      },
+      xAxis: [
+        { // master axis
+          min: 0,
+          title: {
+            enabled: false,
+            text: 'word count'
           },
-        {% endfor %}
-      ] */
-    } /* ,{ // slave axis
-      linkedTo:0 ,
-      minRange: 50,
-      opposite: false,
+          labels: {
+            enabled: false
+          },
+          minRange: 200,
+          startOnTick: false,
+          endOnTick: false,
+          showLastLabel: true,
+          plotBands: DISP.plotBands // NDF:
+          /* plotBands: [
+            {% for key,band in structure %}
+              {
+                from: {{ band.from }},
+                to: {{ band.to }},
+                color: getColor('{{ band.tag }}'),
+                 thickness: '5%'
+              },
+            {% endfor %}
+          ] */
+        }
+        /* ,{ // slave axis
+             linkedTo:0 ,
+             minRange: 50,
+             opposite: false,
+               title: {
+                 enabled: false,
+                   text: 'Essay Structure'
+             },
+             // tickPositions: {{ ticks|json_encode|raw }}, // NDF:
+             labels: {
+               enabled: false,
+                staggerLines: 1,
+                rotation: -90,
+               formatter: function () {
+                 var gg = DISP.formatterGg; // {{ tags|json_encode|raw }} // NDF:
+                 return gg[this.value];},
+               },
+           } */
+      ],
+      yAxis: {
+        minRange: 10,
         title: {
           enabled: false,
-            text: 'Essay Structure'
-      },
-      // tickPositions: {{ ticks|json_encode|raw }}, // NDF:
-      labels: {
-        enabled: false,
-         staggerLines: 1,
-         rotation: -90,
-        formatter: function() {
-          var gg = DISP.formatterGg; // {{ tags|json_encode|raw }} // NDF:
-          return gg[this.value];},
+          text: null
         },
-    } */ ],
-    yAxis: {
-      minRange: 10,
-      title: {
-        enabled: false,
-        text: null
-      },
-      min: 0,
+        min: 0,
 
-      startOnTick: true,
-      endOnTick: true,
-      tickInterval: 1,
-      minorTickInterval: 1,
-      type: 'category',
-      categories: DISP.myCats // myCats, // NDF:
-    },
-    plotOptions: {
-      series: {
-        stickyTracking: false
+        startOnTick: true,
+        endOnTick: true,
+        tickInterval: 1,
+        minorTickInterval: 1,
+        type: 'category',
+        categories: DISP.myCats // myCats, // NDF:
       },
-      scatter: {
-        marker: {
-          symbol: 'diamond',
-          radius: 5,
+      plotOptions: {
+        series: {
+          stickyTracking: false
+        },
+        scatter: {
+          marker: {
+            symbol: 'diamond',
+            radius: 5,
+            states: {
+              hover: {
+                enabled: true,
+                lineColor: 'rgb(100,100,100)'
+              }
+            }
+          },
           states: {
             hover: {
-              enabled: true,
-              lineColor: 'rgb(100,100,100)'
-            }
-          }
-        },
-        states: {
-          hover: {
-            marker: {
-              enabled: false
+              marker: {
+                enabled: false
+              }
             }
           }
         }
-      }
-    },
-    series: DISP.$series // NDF:
+      },
+      series: DISP.$series // NDF:
+    };
+
+    chart = new Highcharts.Chart(chartOptions);
+
+    // $(".highcharts-container svg")
+    //  .prepend("<title>xxxx</title>");
+
+    /* $('.highcharts-legend')
+      .attr('role', "navigation")
+      .attr("aria-label", "Legend")
+      .attr("aria-controls", "container");
+
+    $('.highcharts-legend-item').each(function ()
+      {
+        var cnt = $(this).find("span").html();
+        //if (!cnt) return;
+
+        $(this).attr('role', "checkbox")
+            .attr("title",'Show or hide key words from '+cnt)
+            .attr("tabindex", "0")
+            .click(function () {
+              $(this).focus();
+              });
+      }); */
+
+    $.each(DISP.$series, function (index, item) {
+      // var that = item;
+
+      $('#legend_bullet').append(
+        $('<button/>', {
+          type: 'button',
+          title: 'Show or hide key words from ' + item.name,
+          text: item.name,
+          id: 'legend-tag' + index,
+          'data-tag': item.tag,
+          'class': 'btn active'
+        })
+          .attr('role', 'checkbox')
+          .attr('aria-checked', 'true')
+          .prepend($('<div/>')
+            .addClass('legendcolor')
+            .attr('aria-hidden', 'true')
+            .css('background-color', item.color)
+          ).click(function () {
+            var chart = $('#container').highcharts();
+            var series = chart.series[index];
+
+            $(this).attr('aria-checked', !series.visible);
+
+            if (series.visible) {
+              series.hide();
+            } else {
+              series.show();
+            }
+          }) // click ()
+      ); // append()
+    });
   };
-
-  chart = new Highcharts.Chart(chartOptions);
-
-  // $(".highcharts-container svg")
-  //  .prepend("<title>xxxx</title>");
-
-  /* $('.highcharts-legend')
-    .attr('role', "navigation")
-    .attr("aria-label", "Legend")
-    .attr("aria-controls", "container");
-
-  $('.highcharts-legend-item').each(function()
-    {
-      var cnt = $(this).find("span").html();
-      //if (!cnt) return;
-
-      $(this).attr('role', "checkbox")
-          .attr("title",'Show or hide key words from '+cnt)
-          .attr("tabindex", "0")
-          .click(function() {
-            $(this).focus();
-            });
-    }); */
-
-  $.each(DISP.$series, function (index, item) {
-    var that = item;
-
-    $('#legend_bullet').append(
-      $('<button/>', {
-        type: 'button',
-        title: 'Show or hide key words from ' + item.name,
-        text: item.name,
-        id: 'legend-tag' + index,
-        'data-tag': item.tag,
-        'class': 'btn active'
-      })
-        .attr('role', 'checkbox')
-        .attr('aria-checked', 'true')
-        .prepend($('<div/>')
-          .addClass('legendcolor')
-          .attr('aria-hidden', 'true')
-          .css('background-color', item.color)
-        ).click(function () {
-          var chart = $('#container').highcharts();
-          var series = chart.series[ index ];
-
-          $(this).attr('aria-checked', !series.visible);
-
-          if (series.visible) {
-            series.hide();
-          } else {
-            series.show();
-          }
-        }) // click ()
-    ); // append()
-  });
-
-};
 
 // });
 })(window, window.jQuery, window.openEssayist);
